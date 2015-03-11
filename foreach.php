7823cdae53b34f3bda7b67a5c60877668c52d2d5
J.D. Garza <jdgarza22@yahoo.com>
2015-03-11 12:01:49 -0500

    added foreach.php file

---
+++ foreach.php
@@ -0,0 +1,27 @@
+<?php 
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 11);
+
+foreach ($things as $key => $answer) {
+	if (is_int($answer)) {
+		echo "$answer is an integer\n";
+	}else if(is_string($answer)){
+		echo "$answer is a string\n";
+	}elseif (is_bool($answer)) {
+		echo "$answer is a bool\n";
+	}elseif (is_null($answer)) {
+		echo "$answer is null\n";
+	}elseif (is_array($answer)) {
+		echo "$answer is an array\n";
+	}elseif (is_float($answer)) {
+		echo "$answer is a float\n";
+	}
+
+}
+
+	foreach ($things as $thing => $value) {
+		if (is_scalar($value)) {
+			echo "$value\n";
+		}
+	}
+ ?>
