e21e732d36cae26f3d653803f7d9c61ebdb76146
J.D. Garza <jdgarza22@yahoo.com>
2015-03-11 12:02:39 -0500

    made do_while.php file functional

--- do_while.php
+++ do_while.php
@@ -1,9 +1,18 @@
 <?php 
 
-  $i = 2;
+  $i = 0;
 	do{
-		echo $i.PHP_EOL;
-		$i *= $i;
-	}while ($i < 10000000)
+		
+		if ($i % 15 == 0) {
+			echo "FizzBuzz".PHP_EOL;
+		}else if($i % 3 == 0){
+			echo "Fizz".PHP_EOL;
+		}else if($i % 5 == 0){
+			echo "Buzz".PHP_EOL;
+		}else{
+			echo $i.PHP_EOL;
+		}
+			$i++;
+	}while ($i <= 100)
 
  ?>
